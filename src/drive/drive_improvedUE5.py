fe791e7ccbe73ea697348aa713bca3378ae052f8
Shuai Yuan <zczlsy6@ucl.ac.uk>
2026-05-12 23:32:22 +0800

    try using a fixed vehicle model

--- src/drive/drive_improvedUE5.py
+++ src/drive/drive_improvedUE5.py
@@ -528,7 +528,11 @@ class World(object):
         blueprint_list = get_actor_blueprints(self.world, self._actor_filter, self._actor_generation)
         if not blueprint_list:
             raise ValueError("Couldn't find any blueprints with the specified filters")
-        blueprint = random.choice(blueprint_list)
+        # blueprint = random.choice(blueprint_list)
+
+        # Use a fixed vehicle blueprint -- vehicle.dodge.charger_2020
+        blueprint = self.world.get_blueprint_library().find('vehicle.dodge.charger_2020')
+
         blueprint.set_attribute('role_name', self.actor_role_name)
         if blueprint.has_attribute('terramechanics'):
             blueprint.set_attribute('terramechanics', 'true')
